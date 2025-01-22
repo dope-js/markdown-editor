873d96591a8be8bd020a93392ba1823054a5d8a2
John Zhang <iamjz210@gmail.com>
2025-01-22 13:06:33 +0800

    chore: publish actions

---
+++ scripts/change-version.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const newVersion = process.argv[2].replace(/^v/, ''); // 获取命令行参数中的新版本号,并过滤v字头
+
+if (!newVersion) {
+  console.log('New version missed.');
+  process.exit(1);
+}
+
+const currentDirectory = process.cwd();
+
+const packageJsonPath = path.join(currentDirectory, 'packages', 'editor', 'package.json');
+const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8');
+const packageJson = JSON.parse(packageJsonContent);
+const currentVersion = packageJson.version;
+
+packageJson.version = newVersion;
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+console.log(`The version is changed from ${currentVersion} to ${newVersion}`);
